70e1ce923a9e71d2469426e07a3ec990d5968393
CorentinNrd <corentin.nordmann@epitech.eu>
2022-07-13 20:51:00 +0200

    fix deploy

---
+++ server.js
@@ -0,0 +1,10 @@
+const path = require('path')
+const express = require('express')
+const serveStatic = require('serve-static')
+const app = express()
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+const port = process.env.PORT || 5000
+app.listen(port)
