5cafae18152f39ca116072fb1daa8d5e7b710cb8
CorentinNrd <corentin.nordmann@epitech.eu>
2022-07-13 21:14:15 +0200

    add origin and location for details character

--- src/pages/personnages/[id].vue
+++ src/pages/personnages/[id].vue
@@ -22,16 +22,41 @@ onMounted(() => {
 
 <template>
   <div id="infos">
-    <h1>Info</h1>
+    <img :src="result.image" alt="image du personnage" />
     <p>{{ result.name || 'Nom inconnu' }}</p>
     <p>{{ result.species || 'Espèce inconnue' }}</p>
     <p>{{ result.status || 'Statut inconnu' }}</p>
     <p>{{ result.type || 'Type inconnu' }}</p>
     <p>{{ result.gender || 'Genre inconnu' }}</p>
+    <!-- <span>
+      <p>Liste des épisodes ou il apparaît</p>
+      <ul>
+        <li v-for="(episodes, key) in result.episode" :key="key">
+          {{ episodes }}
+        </li>
+      </ul>
+    </span> -->
+    <span>
+      <p v-for="(v, k) in result.origin" :key="k">{{ v }}</p>
+    </span>
+    <span>
+      <p v-for="(v, k) in result.location" :key="k">{{ v }}</p>
+    </span>
   </div>
 </template>
 
 <style lang="scss">
 #infos {
+  p {
+    color: #fff;
+  }
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+  }
 }
 </style>
